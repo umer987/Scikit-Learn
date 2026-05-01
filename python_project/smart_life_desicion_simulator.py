db3c881fd8f24ac0e5c0641acfa481d5f59968a3
syed umer <umershakir987@gmail.com>
2026-05-01 11:34:12 +0500

    adding python project

--- python_project/smart_life_desicion_simulator.py
+++ python_project/smart_life_desicion_simulator.py
@@ -7,3 +7,14 @@ import random
 # -------------------------------
 # Utility Functions
 # -------------------------------
+
+def get_int_input(prompt):
+    """Safely get integer input"""
+    while True:
+        try:
+            return int(input(prompt))
+        except:
+            print("❌ Invalid input. Enter a number.")
+
+def print_line():
+    print("=" * 50)
